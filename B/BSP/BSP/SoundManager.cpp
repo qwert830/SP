c6ed84b7941c4083df35332657c3f214437aa550
qwert830 <qwertaudrb@naver.com>
2019-05-29 22:15:59 +0900

    2019-05-29 22:15
    fmod 프로젝트 설정 세팅

--- B/BSP/BSP/SoundManager.cpp
+++ B/BSP/BSP/SoundManager.cpp
@@ -16,6 +16,8 @@ void SoundManager::Init()
 	mResult = System_Create(&mSystem);
 
 	mResult = mSystem->init(32, FMOD_INIT_NORMAL, NULL);
-	//mResult = mSystem->createSound
+	
+	mResult = mSystem->createSound("Sound//flaunch.wav", FMOD_LOOP_OFF, 0, &mSound[0]);
+
 
 }
--- B/BSP/BSP/SoundManager.h
+++ B/BSP/BSP/SoundManager.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <fmod.hpp>
 #include <iostream>
-
+#pragma comment(lib, "fmod64_vc.lib")
 using namespace FMOD;
 
 class SoundManager
 {
 private:
 	System *mSystem;
-	Sound  *mSound;
+	Sound  *mSound[10];
 	Channel *mChannel;
 	FMOD_RESULT mResult;
 	
